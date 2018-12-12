cfc2236a84f7ffbd87742c5a76d4acbfdae5aba8
masahiro <aisacart@gmail.com>
2018-12-12 10:56:32 +0900

    migrationファイルに締め切りを示すdeadlineカラムを追加。null false,default now,ヴァリデーションを追記

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 4000 }
+  validates :deadline, presence: true
+
+  def defining_deadline_is_over?
+    errors.add(:deadline, "の日付が作成日時の前に設定されています")
+    self.deadline < self.created_at
+  end
 end
---
+++ db/migrate/20181212013929_add_column_dead_line.rb
@@ -0,0 +1,5 @@
+class AddColumnDeadLine < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :deadline, :datetime, null: false, default: -> { 'NOW()' }
+  end
+end
