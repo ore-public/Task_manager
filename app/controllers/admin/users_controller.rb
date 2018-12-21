2197640176114392ddd6590f78d0e36e3cd64d76
masahiro <aisacart@gmail.com>
2018-12-21 19:14:48 +0900

    admin/users_controllerを修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,7 +15,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_users_path, notice: "ユーザー#{@user.name}を追加しました"
+      redirect_to admin_users_path, notice: "ユーザー：#{@user.name}を追加しました"
     else
       render 'new'
     end
