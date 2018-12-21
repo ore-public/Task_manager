5011a9fef22397ce0fd8ba7a3b85e17e717ceff8
masahiro <aisacart@gmail.com>
2018-12-21 12:30:38 +0900

    adminuserコントローラの記述を追加。new,edit,destroyができるようビューを追加。

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,39 +1,51 @@
-
 class Admin::UsersController < ApplicationController
+  before_action :set_force_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.all.includes(:tasks)
+  end
+
+  def show
   end
 
   def new
-    if logged_in?
-      redirect_to root_path
-    else
-      @user = User.new
-    end
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user.id)
+      redirect_to admin_users_path, notice: "ユーザー#{@user.name}を追加しました"
     else
       render 'new'
     end
   end
 
-   def show
-     @user = User.find(params[:id])
-     redirect_to root_path if @user.id != current_user.id
-   end
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "管理者権限により#{@user.id}:#{@user.name}を編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "管理者権限により#{@user.id}:#{@user.name}を削除しました"
+  end
 
   private
+  def set_force_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user)
           .permit(:name,
                   :email,
-                  :password,
-                  :password_confirmation)
+                  :password)
   end
 end
