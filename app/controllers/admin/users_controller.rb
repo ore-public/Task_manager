886eeb6dfd9c3e00b95f36210934f8e82bb79206
masahiro <aisacart@gmail.com>
2018-12-21 11:05:27 +0900

    adminusercontrollerとインデックスのビューを追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,39 @@
+
+class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    if logged_in?
+      redirect_to root_path
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
+  end
+
+   def show
+     @user = User.find(params[:id])
+     redirect_to root_path if @user.id != current_user.id
+   end
+
+  private
+  def user_params
+    params.require(:user)
+          .permit(:name,
+                  :email,
+                  :password,
+                  :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root to: "tasks#index"
   resources :tasks, :except => [:index]
+  namespace :admin do
+    resources :users
+  end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
