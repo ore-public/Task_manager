34b5a6fa44f8946fdcbbc7ba7af8222ea8f1075d
masahiro <aisacart@gmail.com>
2019-01-09 12:10:49 +0900

    groupmodelにバリデーションを記載。アソシエーションの記述も追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
 class Group < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :info, length: { maximum: 500 }
+
+  belongs_to :user
+  has_many :group_user_relations, dependent: :destroy
+  has_many :joinner, through: :group_user_relations, source: :user
 end
